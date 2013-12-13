d1b8e3ae55971dc5a5a17a50cbaa1a2b66214289
David Estes <davydotcom@gmail.com>
2013-12-13 08:03:06 -0500

    first commit

---
+++ RetinaTagGrailsPlugin.groovy
@@ -0,0 +1,17 @@
+class RetinaTagGrailsPlugin {
+    def version = "0.5.0"
+    def grailsVersion = "2.0 > *"
+    def title = "Retina Tag Plugin"
+    def author = "David Estes"
+    def authorEmail = "destes@bcap.com"
+    def description = "Provides LESS support for the asset-pipeline static asset management plugin."
+    def documentation = "http://github.com/bertramdev/less-grails-asset-pipeline"
+
+    def license = "APACHE"
+    def organization = [ name: "Bertram Capital", url: "http://www.bertramcapital.com/" ]
+    def issueManagement = [ system: "GITHUB", url: "http://github.com/bertramdev/less-grails-asset-pipeline/issues" ]
+    def scm = [ url: "http://github.com/bertramdev/less-grails-asset-pipeline" ]
+
+
+
+}
